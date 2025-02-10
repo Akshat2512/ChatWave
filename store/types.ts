77bcdecf5c989f0c3299e4425c291783939cc2e9
Akshat2512 <akshat.2016603@gmail.com>
2025-02-10 06:27:14 +0530

    version/development

---
+++ store/types.ts
@@ -0,0 +1,6 @@
+
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+export const CREDENTIAL = "CREDENTIAL";
+export const USER_PROFILE_IMG = "USER_PROFILE_IMG";
+export const NAME = "NAME";
